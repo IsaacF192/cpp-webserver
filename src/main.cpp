989be18d227f1b935ffea8b065fde79229dff76f
IsaacF192 <a.forjani2@edu.salford.ac.uk>
2025-04-22 22:04:55 +0100

    modified comments

--- src/main.cpp
+++ src/main.cpp
@@ -362,8 +362,9 @@ void HttpServer::handle_client(int client_fd) {
         
         // If read failed or timed out, close the connection
         logger.log(Logger::WARNING, "Client read timed out or failed");
+        std::cerr << "[Debug] read() error: " << strerror(errno) << std::endl;
 
-        std::cout << "[Debug] Read timed out or failed\n";
+        //std::cout << "[Debug] Read timed out or failed\n";
         close(client_fd);
 
         return;
